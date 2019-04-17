ae42f4b5512d4658378ac34331c093472c7a8602
zhangfaholleword <913272843@qq.com>
2019-04-17 16:03:47 +0800

    环境变量配置

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # vueCli3
+
+## 首次运行说明
+
+```
+$ git clone xxx
+$ npm install
+<!-- 注:如果直接切换分支并拉取代码 需删除原有node_modules -->
+<!-- 注:统一使用node 8.9.3 因为node 8.9.3与node 10.14.2  环境下安装的node-sass会有兼容性问题-->
+<!-- 注:推荐使用nvm node版本管理器-->
+```
+
+## 启动
+```
+$ npm install
+$ npm run serve
+```
+
+## 打包测试环境
+```
+$ npm run test
+```
+
+## 打包正式环境
+```
+$ npm run build
+```
+
+
+
+
+
--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,51 @@
+'use strict'
+
+const pages = {}
+const titles = {
+  name: '主页',
+}
+pages["index"] = {
+  entry: "./src/main.js",
+  template: 'public/index.html',
+  title: titles["name"],
+  chunks: ['chunk-vendors', 'chunk-common', "index"]
+}
+
 module.exports = {
-  // 生产环境是否生成 sourceMap 文件
-  productionSourceMap: false,
-  // css相关配置
-  css: {
-    // 是否使用css分离插件 ExtractTextPlugin
-    extract: true,
-    // 开启 CSS source maps?
-    sourceMap: false,
-    // css预设器配置项
-    loaderOptions: {},
-    // 启用 CSS modules for all css / pre-processor files.
-    modules: false
+  pages: {
+    ...pages
   },
+  // 生产环境是否生成 sourceMap 文件
+  productionSourceMap:
+    false,
+  // css相关配置
+  css:
+    {
+      // 是否使用css分离插件 ExtractTextPlugin
+      extract: true,
+      // 开启 CSS source maps?
+      sourceMap:
+        false,
+      // css预设器配置项
+      loaderOptions:
+        {}
+      ,
+      // 启用 CSS modules for all css / pre-processor files.
+      modules: false
+    }
+  ,
   devServer: {
     proxy: {
-      '/api': {
-        target: 'https://wx.cq.abchina.com/wsq/api',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': ''
+      '/api':
+        {
+          target: 'https://wx.cq.abchina.com/wsq/api',
+          changeOrigin: true,
+          pathRewrite:
+            {
+              '^/api': ''
+            }
         }
-      }
     }
-  },
+  }
+  ,
 }
